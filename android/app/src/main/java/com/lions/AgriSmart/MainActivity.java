72534760623d59183ea24a72b2dea332c5a4c697
nexuz-web <nexuz.in@gmail.com>
2025-09-21 22:24:08 +0530

    baji ka baji kunji

---
+++ android/app/src/main/java/com/lions/AgriSmart/MainActivity.java
@@ -0,0 +1,5 @@
+package com.lions.AgriSmart;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lions.AgriSmart',
+  appName: 'Agri Smart',
+  webDir: 'dist'
+};
+
+export default config;
